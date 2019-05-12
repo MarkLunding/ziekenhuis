3ea9162494ba8afad35309d5c04213fd97025d6a
marklunding <mark@lunding.nl>
2019-05-12 20:20:11 +0200

    Medisch secretaresse toegevoegd

---
+++ src/ziekenhuis/MedischSecretaresse.java
@@ -0,0 +1,17 @@
+package ziekenhuis;
+
+public class MedischSecretaresse extends Personeel {
+
+	boolean isErPlek(Kamer kamer) {
+		// TODO
+		return false;
+
+	}
+
+	boolean kanOperatieDoorgaan() {
+		// TODO
+		return false;
+
+	}
+
+}
