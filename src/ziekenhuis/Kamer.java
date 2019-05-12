455fd4cb26141289e856b6d940b11b853ccd4fbb
EWesthof <e.westhof@workingspirit.nl>
2019-05-12 13:50:48 +0200

    Extra wijzigingen

--- src/ziekenhuis/Kamer.java
+++ src/ziekenhuis/Kamer.java
@@ -1,6 +1,15 @@
 package ziekenhuis;
 
 public abstract class Kamer {
-int aantalBedden;
-int aantalPatienten;
+	int aantalBedden;
+	int aantalPatienten;
+
+	boolean isErPlek() {
+		if (aantalBedden > aantalPatienten) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
 }
--- src/ziekenhuis/Operatiekamer.java
+++ src/ziekenhuis/Operatiekamer.java
@@ -1,8 +1,18 @@
 package ziekenhuis;
 
-public class Operatiekamer extends Kamer implements SterieleRuimte{
-Operatiekamer(){
-	aantalBedden = 1;
-	aantalPatienten = 0;
-}
+public class Operatiekamer extends Kamer implements SterieleRuimte {
+	private boolean steriel = true;
+
+	Operatiekamer() {
+		aantalBedden = 1;
+		aantalPatienten = 0;
+	}
+
+	public boolean isSteriel() {
+		if (steriel = true) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
